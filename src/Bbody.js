e1a2b606a1c6a200eea48bf5b39c71d2ad21b50c
Yousof Khajavi <48098988+Yousoffff@users.noreply.github.com>
2022-06-19 11:36:47 +0430

    grid-container

--- src/Bbody.js
+++ src/Bbody.js
@@ -3,7 +3,18 @@ import './Bbody.css'
 
 
 function Bbody(){
-    return <div className='bbody'><a href="a">hiii</a>hiii</div>
+    return <div className='bbody'>
+        <div className='gridd'>
+            <div className='gridContainer'>
+                <div className='gridC1'>1</div>
+                <div className='gridC2'>2</div>
+                <div className='gridC3'>3</div>
+                <div className='gridC4'>4</div>
+                <div className='gridC5'>5</div>
+                <div className='gridC6'>6</div>
+            </div>
+        </div>
+    </div>
 }
 
 
